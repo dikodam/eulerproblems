d8e469f75bb48611465285928af2ff2a3e41c286
adiko <dikodam@googlemail.com>
2018-12-19 12:18:39 +0100

    multiple function infixed

--- src/main/kotlin/de/dikodam/eulerproblems/problem1/Problem1.kt
+++ src/main/kotlin/de/dikodam/eulerproblems/problem1/Problem1.kt
@@ -6,12 +6,12 @@ package de.dikodam.eulerproblems.problem1
 fun main(args: Array<String>) {
 
     val sumOfMultiplesOf3Or5 = (1..999)
-        .filter { it.isMultipleOf(3) || it.isMultipleOf(5) }
+        .filter { it isMultipleOf 3 || it isMultipleOf 5 }
         .sum()
 
     println("The sum of the multiples of 3 or 5 below 1000 is $sumOfMultiplesOf3Or5.")
 }
 
-fun Int.isMultipleOf(base: Int): Boolean {
+infix fun Int.isMultipleOf(base: Int): Boolean {
     return this % base == 0
 }
--- src/test/kotlin/de/dikodam/eulerproblems/problem1/Problem1KtTest.kt
+++ src/test/kotlin/de/dikodam/eulerproblems/problem1/Problem1KtTest.kt
@@ -1,7 +1,6 @@
 package de.dikodam.eulerproblems.problem1
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
@@ -30,54 +29,8 @@ internal class Problem1KtTest {
 
     @ParameterizedTest
     @MethodSource("testDataProvider")
-    fun `test parametrized data`(data: TestData) {
-        assertThat(data.multiple.isMultipleOf(data.base))
+    fun isMultipleOf(data: TestData) {
+        assertThat(data.multiple isMultipleOf data.base)
             .isEqualTo(data.expectedResult)
-    }
-
-
-    @Test
-    fun `3 is multiple of 3`() {
-        assertThat(3.isMultipleOf(3)).isTrue()
-    }
-
-    @Test
-    fun `4 is not multiple of 3`() {
-        assertThat(4.isMultipleOf(3)).isFalse()
-    }
-
-    @Test
-    fun `5 is not multiple of 3`() {
-        assertThat(4.isMultipleOf(3)).isFalse()
-    }
-
-    @Test
-    fun `6 is multiple of 3`() {
-        assertThat(3.isMultipleOf(3)).isTrue()
-    }
-
-    @Test
-    fun `6 is not multiple of 5`() {
-        assertThat(6.isMultipleOf(5)).isFalse()
-    }
-
-    @Test
-    fun `7 is not multiple of 5`() {
-        assertThat(7.isMultipleOf(5)).isFalse()
-    }
-
-    @Test
-    fun `8 is not multiple of 5`() {
-        assertThat(8.isMultipleOf(5)).isFalse()
-    }
-
-    @Test
-    fun `9 is not multiple of 5`() {
-        assertThat(9.isMultipleOf(5)).isFalse()
-    }
-
-    @Test
-    fun `10 is multiple of 5`() {
-        assertThat(10.isMultipleOf(5)).isTrue()
     }
 }
