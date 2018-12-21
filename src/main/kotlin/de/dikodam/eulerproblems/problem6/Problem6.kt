5c0fbced8a3769de72f01fc2b052d4b6908042ac
adiko <dikodam@googlemail.com>
2018-12-21 11:14:12 +0100

    problem 6 added

---
+++ src/main/kotlin/de/dikodam/eulerproblems/problem6/Problem6.kt
@@ -0,0 +1,8 @@
+package de.dikodam.eulerproblems.problem6
+
+// Find the difference between the sum of the squares of the first one hundred natural numbers
+// and the square of the sum
+fun main(args: Array<String>) {
+
+}
+
